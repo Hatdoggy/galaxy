d940aee22ec8d8ed324b168dbbbb4e63abe8a31c
Hatdoggy <ondaprey@gmail.com>
2021-10-29 07:13:56 +0800

    adjustments

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import { Greet, How, Stats, Terms } from "./Texts";
+import spin, { drop } from "../func";
 import { Spinner } from "./svg";
 import { init } from "../func";
 import { useEffect, useState } from "react";
@@ -9,13 +10,43 @@ const config = {
 };
 
 const Left = (props) => {
-  const { spins, bal, mob, slot, spin } = props;
+  const { spins, bal, mob, slot, spin, pop, upd, size, set } = props;
+  const { welc, lose, win, ctr, show } = pop;
+
+  const click = async () => {
+    let headBtn = document.querySelector(".headBtn");
+    headBtn.style.animation = "";
+    // spin(ctr);
+    await drop();
+    upd({
+      ...pop,
+      show: true,
+      ctr: ctr - 1,
+    });
+  };
+
+  // useEffect(() => {
+  //   set(new Slot(document.getElementById("slot"), config));
+  // }, []);
+
+  // useEffect(() => {
+  //   handleResize();
+  // }, [size]);
+
   return (
     <section className="w-50 h-100 flx flx-col flx-jc-sp flx-ai-ce p-20 fade-l left">
       <h4 className="days txt-wht w-100">GALACTIC SPINNER</h4>
       <Greet />
       <How />
       {!mob && <Stats spins={spins} bal={bal} />}
+      {!mob && (
+        <button
+          className="btn btn-grad w-30 cursor trans spinBtn"
+          onClick={click}
+        >
+          SPIN NOW
+        </button>
+      )}
       {!mob && <Terms />}
     </section>
   );
@@ -67,6 +98,8 @@ const Main = (props) => {
         />
       ) : (
         <Left
+          pop={pop}
+          upd={upd}
           spins={ctr}
           bal={bal}
           mob={mobile}
@@ -78,7 +111,7 @@ const Main = (props) => {
     </main>
   ) : (
     <main className="w-100 h-100 flx">
-      <Left spins={ctr} bal={bal} slot={slot} />
+      <Left spins={ctr} bal={bal} slot={slot} pop={pop} upd={upd} />
       <Svg pop={pop} upd={upd} mob={mobile} size={size} set={set} />
     </main>
   );
